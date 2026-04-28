c7bd8018845e193711d6037d6094e45932b37625
n74600 <chakibalnagib1@gmail.com>
2026-04-29 00:05:06 +0600

    ddfdgffgfg

--- models/Project.js
+++ models/Project.js
@@ -4,9 +4,9 @@ const projectSchema = new mongoose.Schema({
   title: { type: String, required: true },
   image: { type: String, required: true },
   link: { type: String, required: true },
-  tech: { type: String },
-  video: { type: String },
-  description: { type: String, required: true },
+  tech: { type: String }, // Optional
+  video: { type: String }, // Optional
+  description: { type: String }, // Optional (required: true soriye dewa hoyeche)
 }, { timestamps: true });
 
 module.exports = mongoose.model('Project', projectSchema);
